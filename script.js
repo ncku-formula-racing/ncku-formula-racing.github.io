d95d945dfccc2c2149cf030e4507e73b83bbe1f4
Apple <samuellin8lego@gmail.com>
2025-09-23 13:51:42 +0800

    404

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ function toggleCont() {
 
 // version
 
-var version = "v2025.03"; //random lol, maybe cancel this in the future
+var version = "v2025.09"; //random lol, maybe cancel this in the future
 //nvm i changed my mind, now its v[year].[month]
 //var date = "2024/5/6"; //Official release date: idk, maybe change this to lastest updated date
 var date = "";
